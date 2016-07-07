347cdcf025b60d9baf6a52d3b7b679bd16657cd4
$hubh@m <shubhampatwa526@gmail.com>
2016-07-07 18:35:03 +0530

    with login

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,12 +1,15 @@
 var socket = io(); 
-
+// var from="";
 function submitfunction(){
-  var from = $('#user').val();
+  var from = document.getElementById('user').innerHTML;
+  console.log(from);
   var message = $('#m').val();
-  var to=$('#to').val();
-  if(message != '' && from!=to) {
-    $('#messages').append('<li style="display:block;background:#18ffff; float :right"><b style="color:green">' + "Me to "+ to + '</b>: ' + message + '</li><br>');
-    socket.emit('chatMessage',to, from, message);
+  var id=$('#id').val();
+  // var to=$('#to').val();
+  if(message != '' ) {
+    console.log("msg");
+    $('#messages').append('<li style="display:block;background:#18ffff; float :right"><b style="color:green">' + "Me"+ '</b>: ' + message + '</li><br>');
+    socket.emit('chatMessage', id, from, message);
   }
 
   $("#to").val('').focus();
@@ -15,8 +18,8 @@ function submitfunction(){
 }
 
 function notifyTyping() { 
-  var user = $('#user').val();
-  socket.emit('notifyUser', user);
+  // var user = $('#user').val();
+  socket.emit('notifyUser');
 }
 // socket.on('chatMessage', function(from, msg){
   
@@ -30,32 +33,38 @@ function notifyTyping() {
 //   $('#messages').append('<li style="display:block;background:'+backgroundcolor+'; float :'+float+'"><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li><br>');
 // // }
 // });
-socket.on('chatMessage', function(to,from, msg){
-  
-  var me = $('#user').val();
+socket.on('chatMessage', function(from, msg){
+  var me = document.getElementById('user').innerHTML;
+  // var me = $('#user').val();
+  console.log(me,from);
   var color = (from == me) ? 'green' : '#009afd';
   var backgroundcolor=(from == me) ? '#18ffff' : '#4fc3f7';
   var float=(from==me)? 'right' : 'left';
-  var from = (from == me) ? 'Me to'+to : from;
+  var from = (from == me) ? 'Me' : from;
   $('#messages').append('<li style="display:block;background:'+backgroundcolor+'; float :'+float+'"><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li><br>');
 
 });
 
 socket.on('notifyUser', function(user){
-  var me = $('#user').val();
-  if(user != me) {
+  // var me = $('#user').val();
+  // if(user) {
     $('#notifyUser').text(user + ' is typing ...');
   }
-  setTimeout(function(){ $('#notifyUser').text(''); }, 10000);;
-});
+  // setTimeout(function(){ $('#notifyUser').text(''); }, 10000);
+);
 
 $(document).ready(function(){
-  // var name = makeid();
- 
-  var username=window.prompt("please enter name");
-  socket.emit("user",username);
-   $('#user').val(username);
-   document.getElementById('user').innerHTML=username;
+   // var name = <%= id %>;
+   // console.log(name);
+ // alert( );
+ var id=$("#id").val();
+ // from=("#user").val();
+
+ console.log(id);
+  // var username=window.prompt("please enter name");
+  socket.emit("user",id);
+  //  $('#user').val(username);
+  //  document.getElementById('user').innerHTML=username;
  //socket.emit('chatMessage', 'System', '<b>' + username + '</b> has joined the discussion');
 });
 

7444184657dd412c1d81b05f325ce8c691f11392
$hubh@m <shubhampatwa526@gmail.com>
2016-07-08 15:38:16 +0530

    now messages started saving in db

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.use(passport.session());
 var List=mongoose.model('List');
 var User=mongoose.model('User');
 var Room=mongoose.model('Room');
+var Message=mongoose.model('Message');
 
 
 
@@ -105,7 +106,13 @@ app.param('chat',function(req,res,next,chat){
   return next();
 });
 app.get('/chat/:chat',function(req,res,next){
-  res.render('chat',{user:req.user,id:req.chat});
+  Message.find({"room":req.chat},function(err,messages)
+  {
+    if(err)return next(err);
+      console.log(JSON.stringify(messages));
+      return res.render('chat',{user:req.user,id:req.chat,message:messages});
+  }).sort({"date":1})
+  
 })
 
  passport.use(new LocalStrategy(function(username, password, done) {
@@ -176,12 +183,13 @@ io.on('connection', function(socket){
 	socket.on("user",function(_id){
     //console.log(JSON.stringify(req.user1));
     console.log("socket",_id);
-    if(_id in users)
-		  users[_id].push(socket.id);
-    else
-		  users[_id]=[socket.id];
-    // console.log(JSON.stringify(users[_id]));
-	  console.log(users[_id]);	
+   users[socket.id]=_id;
+   //  if(_id in users)
+		 //  users[_id].push(socket.id);
+   //  else
+		 //  users[_id]=[socket.id];
+   //  // console.log(JSON.stringify(users[_id]));
+	  // console.log(users[_id]);	
 		// socket.emit('chatMessage', 'System', '<b>' + username + '</b> has joined the discussion');
 	});
 
@@ -190,14 +198,32 @@ io.on('connection', function(socket){
   // });
   socket.on('chatMessage', function(_id,from,msg){
   	// var id="",selfid="";
-    console.log(from);
-  	for(var i=0;i<users[_id].length;i++)
+    //console.log(from);
+  var query=Room.findById(_id);
+  query.exec(function(err,room){
+    if(err)return next(err);
+    if(!room) return next(new Error('cant found the given room'));
+    var message=new Message();
+    message.senderName=from;
+    message.message=msg;
+    message.room=_id;   
+    message.save(function(err,message)
+    {
+      if (err) {return err;}
+      console.log(JSON.stringify(message._id));
+    });
+  });
+    
+  	for(var i in users)
   	{
-
+        if (users[i]==_id) 
+        {
+          console.log(i," ",users[i]);
+          socket.broadcast.to(i).emit('chatMessage',from, msg);
+  			}
         // console.log("to", users[i].user_name );
-        console.log(users[_id][i]);
-        socket.broadcast.to(users[_id][i]).emit('chatMessage',from, msg);
-  			// console.log("found");
+        // console.log(users[_id][i]);
+        // console.log("found");
   			// break;
   		}
   		// if(users[i].user_name==req.user1)
@@ -214,10 +240,16 @@ io.on('connection', function(socket){
     //socket.broadcast.to(selfid).emit('chatMessage',to,from, msg);
   
   });
-  
-// socket.on('notifyUser', function(){
-//     io.emit('notifyUser', req.user2); 
-//   });
+  socket.on('disconnect',function(){
+    console.log("disconnect",socket.id);
+    delete users[socket.id];
+    for(var i in users){
+      console.log(i," ",users[i]);
+    }
+  })
+  // socket.on('notifyUser', function(){
+  //   io.emit('notifyUser', req.user2); 
+  // });
 });
 
  http.listen(3000, function(){

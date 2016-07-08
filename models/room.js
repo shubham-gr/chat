00592dba2b4eee7b6ad11ae265fac69f62ad3bdf
$hubh@m <shubhampatwa526@gmail.com>
2016-07-08 15:39:17 +0530

    schema corrected

--- models/room.js
+++ models/room.js
@@ -4,7 +4,7 @@ var roomSchema= new mongoose.Schema({
 	user1:String,
 	user2:String,
 	date:{type:Date,default:Date.now},
-	messages:[{type:mongoose.Schema.Types.ObjectId,ref:'Message'}]
+	// messages:[{type:mongoose.Schema.Types.ObjectId,ref:'Message'}]
 });
 
 mongoose.model('Room',roomSchema);

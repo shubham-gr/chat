53e93fdcf7c745109528c30fa82cdeefc1a97e37
$hubh@m <shubhampatwa526@gmail.com>
2016-07-11 15:35:07 +0530

    with logout session end on all tabs

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -21,18 +21,6 @@ function notifyTyping() {
   // var user = $('#user').val();
   socket.emit('notifyUser');
 }
-// socket.on('chatMessage', function(from, msg){
-  
-//   var me = $('#user').val();
-//   // if(from==me || to==me ){
-//   	//console
-//   var color = (from == me) ? 'green' : '#009afd';
-//   var backgroundcolor=(from == me) ? '#18ffff' : '#4fc3f7';
-//   var float=(from==me)? 'right' : 'left';
-//   var from = (from == me) ? 'Me' : from;
-//   $('#messages').append('<li style="display:block;background:'+backgroundcolor+'; float :'+float+'"><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li><br>');
-// // }
-// });
 socket.on('chatMessage', function(from, msg){
   var me = document.getElementById('user').innerHTML;
   // var me = $('#user').val();
@@ -45,27 +33,28 @@ socket.on('chatMessage', function(from, msg){
 
 });
 
+socket.on('sessionEnd',function(){
+  console.log("logout");
+  alert("your session has been expired");
+  window.location.assign("/logout");
+})
+
 socket.on('notifyUser', function(user){
-  // var me = $('#user').val();
-  // if(user) {
     $('#notifyUser').text(user + ' is typing ...');
   }
   // setTimeout(function(){ $('#notifyUser').text(''); }, 10000);
 );
 
 $(document).ready(function(){
-   // var name = <%= id %>;
-   // console.log(name);
- // alert( );
- var id=$("#id").val();
- // from=("#user").val();
+  var id=$("#id").val();
+  var from = document.getElementById('user').innerHTML;
+  console.log(id," ",from);
+  socket.emit("user",id,from);
 
- console.log(id);
-  // var username=window.prompt("please enter name");
-  socket.emit("user",id);
-  //  $('#user').val(username);
-  //  document.getElementById('user').innerHTML=username;
- //socket.emit('chatMessage', 'System', '<b>' + username + '</b> has joined the discussion');
+  $('#logout').click(function(){
+    socket.emit('logout');
+    window.location.assign("/logout");
+  })
 });
 
 
